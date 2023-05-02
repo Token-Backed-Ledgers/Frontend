ea012b076fa96000b52fc2b3b162c4908c017f17
kunlefash <kunlefashmayop@gmail.com>
2023-05-02 21:45:50 +0100

    edit documentation component

--- src/scenes/documentation/index.tsx
+++ src/scenes/documentation/index.tsx
@@ -7,6 +7,7 @@ import {
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 import Documentation from "./Documentation";
+import ActionButton from "@/shared/ActionButton";
 
 const documentations: Array<DocumentationType> = [
   {
@@ -45,7 +46,17 @@ const documentation = ({ setSelectedPage }: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Documentation)}
       >
-        <div className="md:my-5 md:w-3/5">
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 2.0 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           <HText>A TOKEN BACKED LEDGER </HText>
           <p className="my-5 text-sm">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti,
@@ -53,7 +64,7 @@ const documentation = ({ setSelectedPage }: Props) => {
             aspernatur, tempore esse quod aut autem perferendis nisi amet
             voluptates minima excepturi quidem?
           </p>
-        </div>
+        </motion.div>
 
         <motion.div
           className="mt-5 items-center justify-between gap-8 md:flex"
@@ -72,6 +83,59 @@ const documentation = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          <img className="mx-auto" src="" alt="img" />
+          <div>
+            <div>
+              <div className="before:content-abstractwaves before:absolute before:-top-20 before:z-[1] ">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ delay: 0.2, duration: 2.0 }}
+                  variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <HText>
+                    Lorem ipsum dolor sit amet{" "}
+                    <span className="text-primary-500">LOREM</span>
+                  </HText>
+                </motion.div>
+              </div>
+            </div>
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 2.0 }}
+              variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className="my-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Assumenda reiciendis iste cumque magnam cum vitae optio
+                voluptatum quisquam velit nisi ipsa, aspernatur pariatur libero
+                esse? Doloribus ad perspiciatis quaerat numquam?
+              </p>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                pariatur veritatis eligendi quas reiciendis, neque ea aut. A
+                omnis earum dolor, iure voluptatem impedit fuga.
+              </p>
+            </motion.div>
+            <div className="relative mt-16">
+              <div className="before:content-sparkles before:absolute before:-bottom-20 before:right-40 before:z-[-1]">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Learn More
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -50,9 +50,9 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
                   />
                 </div>
                 <div className={`${flexBetween} gap-8`}>
-                  <p>Sign In</p>
+                  <p className=" cursor-pointer">Developer</p>
                   <ActionButton setSelectedPage={setSelectedPage}>
-                    Developer
+                    Sign In
                   </ActionButton>
                 </div>
               </div>
